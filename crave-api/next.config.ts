34e2cf125cb869e1f8661f291990122ef5e37ae0
Matthew Meredith <merematt@udel.edu>
2025-10-22 09:30:17 -0400

    monorepo

---
+++ crave-api/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+import path from "path";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+    turbopack: {
+        root: path.join(import.meta.dirname)
+    }
+};
+
+export default nextConfig;
