4032aa3e5f8d1facd94ca1b07fc10d57209c1ef9
Matthew Meredith <matthewmeredith775@gmail.com>
2025-10-23 12:14:49 -0400

    prettier

--- crave-app/app/index.tsx
+++ crave-app/app/index.tsx
@@ -1,7 +1,6 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-
   return (
     <View
       style={{
@@ -10,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Data: { "unknown"}</Text>
+      <Text>Data: {"unknown"}</Text>
     </View>
   );
 }
