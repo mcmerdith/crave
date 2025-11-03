9105accff3b3640cb99f1fa60c9851593c385a3a
Christian Brandt <christian.brandt@gmail.com>
2025-11-03 10:02:56 -0500

    added data for both carousels

--- crave-app/app/index.tsx
+++ crave-app/app/index.tsx
@@ -22,19 +22,49 @@ export default function Index() {
       />
       <Carousel
         title="Recent Cravings"
-        data={DATA}
+        data={RecentsData}
         onViewAll={() => console.log("View All pressed")}
       />
       <Carousel
-        title="Discover Nearby"
-        data={DATA}
+        title="Discover New"
+        data={DiscoverData}
         onViewAll={() => console.log("View All pressed")}
       />
     </SafeAreaView>
   );
 }
 
-const DATA = [
+const RecentsData = [
+  {
+    name: "Taverna",
+    cuisine: "Italian",
+    rating: 4.8,
+    distance: "0.5 mi",
+    price: "$$",
+    image:
+      "https://vrconcierge.com/wp-content/uploads/2021/02/taverna-rustic-italian-newark-de-exterior-1-768x512.jpg",
+  },
+  {
+    name: "El Diablo",
+    cuisine: "Mexican",
+    rating: 4.4,
+    distance: "0.5 mi",
+    price: "$",
+    image:
+      "https://images.squarespace-cdn.com/content/v1/58b57e8b2e69cffff969c6cd/1488299173082-81E2GCSB63YW66RKF8HO/Burrito_wood_retouched.jpg?format=1500w",
+  },
+  {
+    name: "m2o Burger",
+    cuisine: "American",
+    rating: 4.9,
+    distance: "0.4 mi",
+    price: "$",
+    image:
+      "https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_1200,q_auto,fl_lossy,dpr_auto,c_fill,f_auto,h_800,g_auto/wtisrayz07qylnbwba6e",
+  },
+];
+
+const DiscoverData = [
   {
     name: "Taverna",
     cuisine: "Italian",
