4904e278023d78445e97f400a5359fe1532502d5
Matthew Meredith <matthewmeredith775@gmail.com>
2025-11-21 21:15:47 -0500

    Make types export actually good

---
+++ crave-api/rollup.types.config.mjs
@@ -0,0 +1,14 @@
+import { dts } from "rollup-plugin-dts";
+
+const config = {
+  input: './dist/server/api/types.d.ts',
+
+  output: {
+    file: './api-types.d.ts',
+    format: 'es'
+  },
+
+  plugins: [dts()],
+};
+
+export default config;
