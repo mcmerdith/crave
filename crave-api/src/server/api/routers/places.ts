862c2336273dffcc1c96f73097f61b257f6a328f
Matthew Meredith <matthewmeredith775@gmail.com>
2025-11-23 14:57:25 -0500

    Public surface for Places API

--- crave-api/src/server/api/routers/places.ts
+++ crave-api/src/server/api/routers/places.ts
@@ -1,63 +1,27 @@
 import { publicProcedure, router } from "../trpc";
-import { PlacesClient } from "@googlemaps/places";
-import { Client } from "@googlemaps/google-maps-services-js";
-import { env } from "@/env";
-import { Place } from "../types";
-
-const places = new PlacesClient({
-  apiKey: env.GOOGLE_API_KEY,
-});
-const maps = new Client();
-
-function fieldMask(fields: string[]) {
-  return fields.map((f) => `places.${f}`).join(",")
-}
-
-function toMeters(miles: number) {
-  return miles * 1609.34;
-}
-
-async function getCoordinates(query: string) {
-  const data = await maps.geocode({
-    params: {
-      address: query,
-      key: env.GOOGLE_API_KEY,
-    },
-  });
-  // Return the data formatted for the places api
-  const { lat, lng } = data.data.results[0].geometry.location;
-  console.log("geocoded to", lat, lng)
-  return {
-    latitude: lat,
-    longitude: lng,
-  }
-}
-
-async function search() {
-  const [data] = await places.searchNearby({
-    includedPrimaryTypes: ["restaurant"],
-    locationRestriction: {
-      circle: {
-        center: await getCoordinates("Newark, DE"),
-        radius: toMeters(5)
-      },
-    },
-
-  }, {
-    otherArgs: {
-      headers: {
-        "X-Goog-FieldMask": fieldMask(["name", "displayName", "types", "currentOpeningHours"])
-      }
-    }
-  });
-  if (!data.places) return [];
-  return data.places;
-}
+import {
+  autocomplete,
+  getAutocompleteCoordinates,
+  searchPlaces,
+} from "@/server/api/maps-api";
+import { SearchPlacesParams } from "@/server/api/types/places";
+import {
+  AutocompleteParams,
+  GetAutocompleteCoordinatesParams,
+} from "@/server/api/types/autocomplete";
 
 export const placesRouter = router({
-  list: publicProcedure.output(Place.array()).query(async () => {
-    // const res = await search()
-    return [];
+  search: publicProcedure.input(SearchPlacesParams).query(async ({ input }) => {
+    return await searchPlaces(input);
   }),
-  // TODO
+  autocomplete: publicProcedure
+    .input(AutocompleteParams)
+    .query(async ({ input }) => {
+      return await autocomplete(input);
+    }),
+  getAutocompleteCoordinates: publicProcedure
+    .input(GetAutocompleteCoordinatesParams)
+    .query(async ({ input }) => {
+      return await getAutocompleteCoordinates(input);
+    }),
 });
