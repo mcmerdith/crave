c77e33b4701c6dfe981a71dfdca479b3be092672
Matthew Meredith <matthewmeredith775@gmail.com>
2025-12-04 00:16:23 -0500

    why slash

--- crave-app/config.ts
+++ crave-app/config.ts
@@ -1,2 +1,2 @@
 // If developing locally, set to the local IP and port of the API server
-export const TRPC_HOST = "https://crave-api-rho.vercel.app/";
+export const TRPC_HOST = "https://crave-api-rho.vercel.app";
